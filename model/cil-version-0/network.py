719aa52a9a577fb8dd2f04ac6e96198e1b61b0a9
Yongqi WANG <wangyq977@gmail.com>
2019-06-07 19:54:22 +0200

    Update network.py

--- model/cil-version-0/network.py
+++ model/cil-version-0/network.py
@@ -1,180 +1,95 @@
-# encoding: utf-8
-
+#===================================================================================#
+# Computational Intelligence Lab(CIL) ETHZ Sp19 road segmentation competition		#
+# Team Member: Yongqi Wang, Zhi Ye, Jingyuan Ma										#
+# Author: Jingyuan Ma																#
+# Version: v1.0																		#	
+# Description: In this file, main network structure is written in this file.		#
+#			   This is version v1.0 with network constructed with plain Convolution #
+#			   Convolution Dimension is 											#
+#			   loss is returned if training;otherwise, classfication				#
+#===================================================================================#
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from torch.utils.checkpoint import checkpoint
-# from torchvision.models import resnet50, resnet101, resnet152
+from torch.nn.modules.batchnorm import BatchNorm2d as BN2D
 
 from config import config
-from base_model import resnet18
-from seg_opr.seg_oprs import ConvBnRelu, AttentionRefinement, FeatureFusion
 
 
 def get():
-    return BiSeNet(config.num_classes, None, None)
+	return Network_v1(config.num_classes,None,None)
 
-
-class BiSeNet(nn.Module):
-    def __init__(self, out_planes, is_training,
-                 criterion, ohem_criterion, pretrained_model=None,
-                 norm_layer=nn.BatchNorm2d):
-        super(BiSeNet, self).__init__()
-        self.context_path = resnet18(pretrained_model, norm_layer=norm_layer,
-                                     bn_eps=config.bn_eps,
-                                     bn_momentum=config.bn_momentum,
-                                     deep_stem=False, stem_width=64)
-
-        self.business_layer = []
-        self.is_training = is_training
-
-        self.spatial_path = SpatialPath(3, 128, norm_layer)
-
-        conv_channel = 128
-        self.global_context = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            ConvBnRelu(512, conv_channel, 1, 1, 0,
+class Network_v1(nn.Module):
+	def __init__(self, out_planes, is_training, pretrained_model=None):
+        super(Network_v1, self).__init__()
+		conv_channel = 128
+		
+		self.layes = []
+		#conv7x7 with stride 1 padding 1, increase depth
+		self.conv1 = ConvBnRelu(1, conv_channel, 7, 1, 3,
                        has_bn=True,
                        has_relu=True, has_bias=False, norm_layer=norm_layer)
         )
+		##conv7x7 with stride 1 padding 1, downsampling by 2
+		self.conv2 = ConvBnRelu(conv_channel , conv_channel, 7, 2, 3,
+                       has_bn=True,
+                       has_relu=True, has_bias=False, norm_layer=norm_layer)
+        )
+		#conv3x3 with stride 1 padding 1, increase depth
+		self.conv3 = ConvBnRelu(conv_channel, conv_channel*2, 3, 1, 1,
+                       has_bn=True,
+                       has_relu=True, has_bias=False, norm_layer=norm_layer)
+        )
+		#conv3x3 with stride 1 padding 1, downsampling by 2
+		self.conv4 = ConvBnRelu(conv_channel*2, conv_channel*2, 3, 2, 1,
+                       has_bn=True,
+                       has_relu=True, has_bias=False, norm_layer=norm_layer)
+        )
+		#conv1x1 with stride 1 padding 1, reduce depth for classification
+		self.conv5 = ConvBnRelu(conv_channel*2, outplanes, 3, 2, 1,
+                       has_bn=True,
+                       has_relu=True, has_bias=False, norm_layer=norm_layer)
+        )
+		self.loss = nn.criterion = nn.CrossEntropyLoss(reduction='mean',
+                                    ignore_index=255)
+		
+	def forward(self, x, gt):
+		x = self.conv1(x)
+		x = self.conv2(x)
+		x = self.conv3(x)
+		x = self.conv4(x)
+		x = self.conv5(x)
 
-        # stage = [512, 256, 128, 64]
-        arms = [AttentionRefinement(512, conv_channel, norm_layer),
-                AttentionRefinement(256, conv_channel, norm_layer)]
-        refines = [ConvBnRelu(conv_channel, conv_channel, 3, 1, 1,
-                              has_bn=True, norm_layer=norm_layer,
-                              has_relu=True, has_bias=False),
-                   ConvBnRelu(conv_channel, conv_channel, 3, 1, 1,
-                              has_bn=True, norm_layer=norm_layer,
-                              has_relu=True, has_bias=False)]
+		if is_training:
+			loss = self.loss(x,gt)
+			return loss
+			
+		return F.log_softmax(x, dim=1)
 
-        if is_training:
-            heads = [BiSeNetHead(conv_channel, out_planes, 2,
-                                 True, norm_layer),
-                     BiSeNetHead(conv_channel, out_planes, 1,
-                                 True, norm_layer),
-                     BiSeNetHead(conv_channel * 2, out_planes, 1,
-                                 False, norm_layer)]
-        else:
-            heads = [None, None,
-                     BiSeNetHead(conv_channel * 2, out_planes, 1,
-                                 False, norm_layer)]
-
-        self.ffm = FeatureFusion(conv_channel * 2, conv_channel * 2,
-                                 1, norm_layer)
-
-        self.arms = nn.ModuleList(arms)
-        self.refines = nn.ModuleList(refines)
-        self.heads = nn.ModuleList(heads)
-
-        self.business_layer.append(self.spatial_path)
-        self.business_layer.append(self.global_context)
-        self.business_layer.append(self.arms)
-        self.business_layer.append(self.refines)
-        self.business_layer.append(self.heads)
-        self.business_layer.append(self.ffm)
-
-        if is_training:
-            self.criterion = criterion
-            self.ohem_criterion = ohem_criterion
-
-    def forward(self, data, label=None):
-        spatial_out = self.spatial_path(data)
-
-        context_blocks = self.context_path(data)
-        context_blocks.reverse()
-
-        global_context = self.global_context(context_blocks[0])
-        global_context = F.interpolate(global_context,
-                                       size=context_blocks[0].size()[2:],
-                                       mode='bilinear', align_corners=True)
-
-        last_fm = global_context
-        pred_out = []
-
-        for i, (fm, arm, refine) in enumerate(zip(context_blocks[:2], self.arms,
-                                                  self.refines)):
-            fm = arm(fm)
-            fm += last_fm
-            last_fm = F.interpolate(fm, size=(context_blocks[i + 1].size()[2:]),
-                                    mode='bilinear', align_corners=True)
-            last_fm = refine(last_fm)
-            pred_out.append(last_fm)
-        context_out = last_fm
-
-        concate_fm = self.ffm(spatial_out, context_out)
-        pred_out.append(concate_fm)
-
-        if self.is_training:
-            aux_loss0 = self.ohem_criterion(self.heads[0](pred_out[0]), label)
-            aux_loss1 = self.ohem_criterion(self.heads[1](pred_out[1]), label)
-            main_loss = self.ohem_criterion(self.heads[-1](pred_out[2]), label)
-
-            loss = main_loss + aux_loss0 + aux_loss1
-            return loss
-
-        return F.log_softmax(self.heads[-1](pred_out[-1]), dim=1)
-
-
-class SpatialPath(nn.Module):
-    def __init__(self, in_planes, out_planes, norm_layer=nn.BatchNorm2d):
-        super(SpatialPath, self).__init__()
-        inner_channel = 64
-        self.conv_7x7 = ConvBnRelu(in_planes, inner_channel, 7, 2, 3,
-                                   has_bn=True, norm_layer=norm_layer,
-                                   has_relu=True, has_bias=False)
-        self.conv_3x3_1 = ConvBnRelu(inner_channel, inner_channel, 3, 2, 1,
-                                     has_bn=True, norm_layer=norm_layer,
-                                     has_relu=True, has_bias=False)
-        self.conv_3x3_2 = ConvBnRelu(inner_channel, inner_channel, 3, 2, 1,
-                                     has_bn=True, norm_layer=norm_layer,
-                                     has_relu=True, has_bias=False)
-        self.conv_1x1 = ConvBnRelu(inner_channel, out_planes, 1, 1, 0,
-                                   has_bn=True, norm_layer=norm_layer,
-                                   has_relu=True, has_bias=False)
+class ConvBnRelu(nn.Module):
+    def __init__(self, in_planes, out_planes, ksize, stride, pad, dilation=1,
+                 groups=1, has_bn=True, norm_layer=nn.BatchNorm2d, bn_eps=1e-5,
+                 has_relu=True, inplace=True, has_bias=False):
+        super(ConvBnRelu, self).__init__()
+        self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=ksize,
+                              stride=stride, padding=pad,
+                              dilation=dilation, groups=groups, bias=has_bias)
+        self.has_bn = has_bn
+        if self.has_bn:
+            self.bn = norm_layer(out_planes, eps=bn_eps)
+        self.has_relu = has_relu
+        if self.has_relu:
+            self.relu = nn.ReLU(inplace=inplace)
 
     def forward(self, x):
-        x = self.conv_7x7(x)
-        x = self.conv_3x3_1(x)
-        x = self.conv_3x3_2(x)
-        output = self.conv_1x1(x)
+        x = self.conv(x)
+        if self.has_bn:
+            x = self.bn(x)
+        if self.has_relu:
+            x = self.relu(x)
 
-        return output
-
-
-class BiSeNetHead(nn.Module):
-    def __init__(self, in_planes, out_planes, scale,
-                 is_aux=False, norm_layer=nn.BatchNorm2d):
-        super(BiSeNetHead, self).__init__()
-        if is_aux:
-            self.conv_3x3 = ConvBnRelu(in_planes, 128, 3, 1, 1,
-                                       has_bn=True, norm_layer=norm_layer,
-                                       has_relu=True, has_bias=False)
-        else:
-            self.conv_3x3 = ConvBnRelu(in_planes, 64, 3, 1, 1,
-                                       has_bn=True, norm_layer=norm_layer,
-                                       has_relu=True, has_bias=False)
-        # self.dropout = nn.Dropout(0.1)
-        if is_aux:
-            self.conv_1x1 = nn.Conv2d(128, out_planes, kernel_size=1,
-                                      stride=1, padding=0)
-        else:
-            self.conv_1x1 = nn.Conv2d(64, out_planes, kernel_size=1,
-                                      stride=1, padding=0)
-        self.scale = scale
-
-    def forward(self, x):
-        fm = self.conv_3x3(x)
-        # fm = self.dropout(fm)
-        output = self.conv_1x1(fm)
-        if self.scale > 1:
-            output = F.interpolate(output, scale_factor=self.scale,
-                                   mode='bilinear',
-                                   align_corners=True)
-
-        return output
-
-
-if __name__ == "__main__":
-    model = BiSeNet(19, None)
-    # print(model)
+        return x
+		
+		
