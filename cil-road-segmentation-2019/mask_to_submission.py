0f7f0e6b4c4ba225f8c8d31ecaeb2bae8ce39f76
wyq977 <wangyq977@gmail.com>
2019-06-14 13:02:29 +0000

    clean repo

--- cil-road-segmentation-2019/mask_to_submission.py
+++ cil-road-segmentation-2019/mask_to_submission.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import matplotlib.image as mpimg
 import re
+import glob
 
 foreground_threshold = 0.25 # percentage of pixels > 1 required to assign a foreground label to a patch
 
@@ -37,10 +38,10 @@ def masks_to_submission(submission_filename, *image_filenames):
 
 
 if __name__ == '__main__':
-    submission_filename = 'dummy_submission.csv'
+    submission_filename = 'submission.csv'
     image_filenames = []
-    for i in range(1, 51):
-        image_filename = 'training/groundtruth/satImage_' + '%.3d' % i + '.png'
-        print image_filename
-        image_filenames.append(image_filename)
+    for i in glob.glob('test_pred/*png'):
+        image_filenames.append(i)
+        print(i)
+
     masks_to_submission(submission_filename, *image_filenames)
