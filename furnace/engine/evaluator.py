9bae634e18cfd89f7180e8c281bf2dc2c237f042
Yongqi WANG <wangyq977@gmail.com>
2019-06-18 19:12:17 +0200

    delete dist from evaluator

--- furnace/engine/evaluator.py
+++ furnace/engine/evaluator.py
@@ -19,7 +19,7 @@ logger = get_logger()
 class Evaluator(object):
     def __init__(self, dataset, class_num, image_mean, image_std, network,
                  multi_scales, is_flip, devices,
-                 verbose=False, save_path=None, show_image=False):
+                 verbose=False, save_path=None):
         self.dataset = dataset
         self.ndata = self.dataset.get_length()
         self.class_num = class_num
@@ -38,42 +38,11 @@ class Evaluator(object):
         self.save_path = save_path
         if save_path is not None:
             ensure_dir(save_path)
-        self.show_image = show_image
 
     def run(self, model_path, model_indice, log_file, log_file_link):
-        """There are four evaluation modes:
-            1.only eval a .pth model: -e *.pth
-            2.only eval a certain epoch: -e epoch
-            3.eval all epochs in a given section: -e start_epoch-end_epoch
-            4.eval all epochs from a certain started epoch: -e start_epoch-
-            """
+        """Evaluate models."""
         if '.pth' in model_indice:
             models = [model_indice, ]
-        elif "-" in model_indice:
-            start_epoch = int(model_indice.split("-")[0])
-            end_epoch = model_indice.split("-")[1]
-
-            models = os.listdir(model_path)
-            models.remove("epoch-last.pth")
-            sorted_models = [None] * len(models)
-            model_idx = [0] * len(models)
-
-            for idx, m in enumerate(models):
-                num = m.split(".")[0].split("-")[1]
-                model_idx[idx] = num
-                sorted_models[idx] = m
-            model_idx = np.array([int(i) for i in model_idx])
-
-            down_bound = model_idx >= start_epoch
-            up_bound = [True] * len(sorted_models)
-            if end_epoch:
-                end_epoch = int(end_epoch)
-                assert start_epoch < end_epoch
-                up_bound = model_idx <= end_epoch
-            bound = up_bound * down_bound
-            model_slice = np.array(sorted_models)[bound]
-            models = [os.path.join(model_path, model) for model in
-                      model_slice]
         else:
             models = [os.path.join(model_path,
                                    'epoch-%s.pth' % model_indice), ]
@@ -162,9 +131,7 @@ class Evaluator(object):
                               (output_size[1], output_size[0]),
                               interpolation=cv2.INTER_LINEAR)
 
-        #print("Pred shape:", pred.shape)
-        #print(pred)
-        pred = pred.argmax(2) # change from 2 to 1
+        pred = pred.argmax(2)
 
         return pred
 
