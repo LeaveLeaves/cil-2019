344a7572aeed0e04371a012686bb17d6bd49431b
yezhi <fly9811@163.com>
2019-06-18 16:37:14 +0200

    Update extant_file

--- furnace/utils/pyt_utils.py
+++ furnace/utils/pyt_utils.py
@@ -95,6 +95,15 @@ def parse_devices(input_devices):
 
     return devices
 
+def extant_file(x):
+    """
+    'Type' for argparse - checks that file exists but does not open.
+    """
+    if not os.path.exists(x):
+        # Argparse uses the ArgumentTypeError to give a rejection message like:
+        # error: argument input: x does not exist
+        raise argparse.ArgumentTypeError("{0} does not exist".format(x))
+    return x
 
 def link_file(src, target):
     if os.path.isdir(target) or os.path.isfile(target):
