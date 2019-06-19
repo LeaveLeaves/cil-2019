1a233c05a8447ca827a50cdeec9498f3f77e1b12
wyq977 <wangyq977@gmail.com>
2019-06-19 16:20:54 +0200

    change source_suffix

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 master_doc = 'index'
-source_suffix = '.rst'
+# source_suffix = '.rst'
 
 
 # -- Project information -----------------------------------------------------
