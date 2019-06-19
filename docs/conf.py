23324a3721ed86dc3406ddde021c6b999cac9f3a
wyq977 <wangyq977@gmail.com>
2019-06-19 14:54:05 +0200

    change master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import sys
 # sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath('.') ), 'furnace') )
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
+master_doc = 'index'
 
 # -- Project information -----------------------------------------------------
 
