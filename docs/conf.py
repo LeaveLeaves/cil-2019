5fa3a0fbd0e4b51734488027a6cc38a554ba7f38
wyq977 <wangyq977@gmail.com>
2019-06-19 16:10:00 +0200

    change style

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,8 @@ import sys
 # sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../'))
 master_doc = 'index'
+source_suffix = '.rst'
+
 
 # -- Project information -----------------------------------------------------
 
@@ -29,7 +31,7 @@ author = 'Jingyuan Ma, Ye Zhi, Yongqi Wang'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'recommonmark','sphinx.ext.todo','sphinx.ext.githubpages',
 ]
  
 napoleon_google_docstring = False
@@ -63,7 +65,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'classic'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
